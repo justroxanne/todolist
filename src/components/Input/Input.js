e6927a7cc879d975818fecd28b006f75cd85238a
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-04-10 20:35:17 +0200

    header and title done

---
+++ src/components/Input/Input.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Input.css';
+
+const Input = () => {
+    return <input type='text' id='todo-input' name='todo-input'></input>
+}
+
+export default Input;
---
+++ src/components/Title/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './Title.css';
+
+
+const Title = () => {
+    return <h1 className='title'>What do you have to do?</h1>
+};
+
+export default Title;
